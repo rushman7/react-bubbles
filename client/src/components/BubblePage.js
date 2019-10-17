1e9716cac445e3808cb89856be75c58319612f1b
BuildTools <arthurpisakhov1@gmail.com>
2019-10-17 16:45:45 -0400

    BubblePage

--- client/src/components/BubblePage.js
+++ client/src/components/BubblePage.js
@@ -1,19 +1,27 @@
 import React, { useState, useEffect } from "react";
-import axios from "axios";
+import { axiosWithAuth } from '../utils/axiosWithAuth';
 
 import Bubbles from "./Bubbles";
 import ColorList from "./ColorList";
 
 const BubblePage = () => {
   const [colorList, setColorList] = useState([]);
-  // fetch your colors data from the server when the component mounts
-  // set that data to the colorList state property
+  
+  useEffect(() => {
+    axiosWithAuth()
+    .get('/colors')
+    .then(res => {
+      console.log(res)
+      setColorList(res.data)
+    })
+    .catch(err => console.log(err))
+  }, [])
 
   return (
-    <>
+    <div>
       <ColorList colors={colorList} updateColors={setColorList} />
       <Bubbles colors={colorList} />
-    </>
+    </div>
   );
 };
 
